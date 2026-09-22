b92536b1f2b29e956ce8a445ecdcfb077e6cd317
lareii <lareii@proton.me>
2026-09-22 23:26:26 +0300

    fix jule formula

--- Formula/jule.rb
+++ Formula/jule.rb
@@ -1,16 +1,14 @@
 class Jule < Formula
   desc "Safe systems programming language with C and C++ interoperability"
   homepage "https://jule.dev"
+  url "https://github.com/julelang/jule/releases/download/jule0.2.2/jule0.2.2-darwin-arm64.tar.xz"
   version "0.2.2"
+  sha256 "842a8427c24bebc19df02c4aea7fede7a1e436f44eca279518cd83365549c773"
   license "BSD-3-Clause"
 
   depends_on :macos
 
   on_macos do
-    on_arm do
-      url "https://github.com/julelang/jule/releases/download/jule0.2.2/jule0.2.2-darwin-arm64.tar.xz"
-      sha256 "842a8427c24bebc19df02c4aea7fede7a1e436f44eca279518cd83365549c773"
-    end
     on_intel do
       url "https://github.com/julelang/jule/releases/download/jule0.2.2/jule0.2.2-darwin-amd64.tar.xz"
       sha256 "37643bbe11668989f5ab0eba54db0388cbdbcdcbb30fd6cabf13605656a0038b"
